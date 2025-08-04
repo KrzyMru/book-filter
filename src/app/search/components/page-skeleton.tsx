59e971558777da661f9bbb3cda6e93a732d28168
KrzyMru <krzymru20@gmail.com>
2025-08-04 16:08:38 +0200

    Changed loading page background

--- src/app/search/components/page-skeleton.tsx
+++ src/app/search/components/page-skeleton.tsx
@@ -8,12 +8,9 @@ const PageSkeleton = () => {
                     <div className="h-[32px] bg-gray-200 animate-pulse mb-1" />
                     <div className="h-[16px] bg-gray-200 animate-pulse" />
                 </div>
-                <ul className="grid gap-[2px] bg-gray-300 lg:grid-cols-2 2xl:grid-cols-3">
+                <ul className="grid lg:grid-cols-2 2xl:grid-cols-3">
                     {Array.from({ length: 10 }, (_, i) => (
-                        <li 
-                            key={i} 
-                            className="bg-white"
-                        >
+                        <li key={i}>
                             <WorkSnippetRectangleSkeleton />
                         </li>
                     ))}
